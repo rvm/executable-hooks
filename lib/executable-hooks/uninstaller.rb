1db37193dcbfd969b786c7d573d02802fb31a5d6
Michal Papis <mpapis@gmail.com>
2013-07-11 00:39:54 +0200

    honor program_suffix in uninstaller, update #3

--- lib/executable-hooks/uninstaller.rb
+++ lib/executable-hooks/uninstaller.rb
@@ -3,7 +3,7 @@ require 'executable-hooks/regenerate_binstubs_command'
 
 module ExecutableHooks
   def self.uninstall
-    Gem.configuration[:custom_shebang] = '$env ruby'
+    Gem.configuration[:custom_shebang] = "$env #{Gem.default_exec_format % "ruby"}"
     RegenerateBinstubsCommand.new.execute_no_wrapper
     Wrapper.uninstall
   end
